b4445aaaa0c1a6f0c57bca3b8de097b322d1f0ea
NOOB-3301 <sepic466@gmail.com>
2025-01-01 11:52:37 +0530

    added comments

--- server/api/dbconnect.js
+++ server/api/dbconnect.js
@@ -7,6 +7,10 @@ dotenv.config({
 
 const url = process.env.MONGO_URI
 console.log(url)
+if(!url){
+    console.error("now url received from env, chek path of env file")
+    process.exit()
+}
 
 export const dbConnect =async() =>{
     const resp = await mongoose.connect(url)
