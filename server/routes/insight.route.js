73eb203b8d43e0111716af195c9300f791f805bf
NOOB-3301 <sepic466@gmail.com>
2025-01-01 15:43:34 +0530

    vercel fixing 2

--- server/routes/insight.route.js
+++ server/routes/insight.route.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ dest: "/temp" }); 
+const upload = multer({ dest: "/tmp" }); 
 const router = Router()
 
 router.route('/addinsight').post( upload.single('imagefile'), addInsight)
