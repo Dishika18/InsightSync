65c6833d4c9000b49302668aa0944dbf82c1694d
ANSHIKA-26 <arora.anshika.26@gmail.com>
2025-01-03 18:42:23 +0530

    added scroll to top functionality

--- js/custom.js
+++ js/custom.js
@@ -51,3 +51,17 @@
   })(window.jQuery);
 
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
