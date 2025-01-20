4f392e292e78e9c6e79c8b9accab1a10176b1feb
Dishika Vaishkiyar <152963337+Dishika18@users.noreply.github.com>
2025-01-20 23:44:19 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 import { dbConnect } from './dbconnect.js';
 import { app } from './app.js';
 
-
+// Load environment variables
 dotenv.config();
 
 console.log('Starting app. MONGO_URI:', process.env.MONGO_URI);
 
-
+// Connect to the database and start the server
 dbConnect()
   .then(() => {
     const PORT = process.env.PORT || 3000;
@@ -18,4 +18,3 @@ dbConnect()
   .catch((err) => {
     console.error('Database connection failed:', err);
   });
-
